b84f08c473ef572c76567962dfb8b485358e24b6
Juliana Gonzalez-Astudillo <julianagonzalezastudillo@gmail.com>
2026-03-25 23:01:49 -0400

    add latency to slow wave plotting to plot_epoch

--- src/utils/plotting.py
+++ src/utils/plotting.py
@@ -84,6 +84,7 @@ def plot_epoch(epoch, sfreq, spike_idx=None, mad=None, k=None, active=None,
                slow_wave_idx=None,
                slow_wave=None,
                slow_wave_duration=None,
+               latency=None,
                title="IED Epoch"):
     # Plot a single epoch.
     times = np.arange(len(epoch)) / sfreq  # time in seconds
@@ -119,7 +120,7 @@ def plot_epoch(epoch, sfreq, spike_idx=None, mad=None, k=None, active=None,
     # Slow wave as colored segment
     if slow_wave_idx is not None and len(slow_wave_idx) > 0:
         ax.plot(times[slow_wave_idx], epoch[slow_wave_idx],
-                color='green', linewidth=2.5, alpha=0.7, label='Slow wave')
+                color='green', linewidth=2.5, alpha=0.5, label='Slow wave')
 
     # Filtered slow signal (overlay)
     if slow_wave is not None and slow_wave_idx is not None:
@@ -161,6 +162,31 @@ def plot_epoch(epoch, sfreq, spike_idx=None, mad=None, k=None, active=None,
         ax.text((times[start] + times[end]) / 2,
                 y_arrow,
                 'duration',
+                color='black',
+                ha='center',
+                va='bottom')
+
+    # Latency as arrow <-->
+    if latency is not None:
+        start = spike_idx
+        end = spike_idx + latency
+
+        # Threshold where duration is defined
+        slow_peak_idx = np.argmax(np.abs(slow_wave))
+        slow_amp = np.abs(slow_wave[slow_peak_idx])
+
+        # Arrow
+        ax.annotate(
+            '',
+            xy=(times[end], slow_amp),
+            xytext=(times[start], slow_amp),
+            arrowprops=dict(arrowstyle='<->', color='black', lw=1.5)
+        )
+
+        # Label
+        ax.text((times[start] + times[end]) / 2,
+                slow_amp,
+                'latency',
                 color='black',
                 ha='center',
                 va='bottom')
